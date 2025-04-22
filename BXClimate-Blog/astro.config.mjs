598c47dd546968ea2c8fc92720b01a2813a47379
Steve Derico <steve@bixbyapps.com>
2025-04-22 10:03:57 -0700

    up

--- BXClimate-Blog/astro.config.mjs
+++ BXClimate-Blog/astro.config.mjs
@@ -8,9 +8,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://climate.bixbyapps.com',
   integrations: [mdx(), sitemap(), react()],
-
+  base: '/blog',
   vite: {
     plugins: [tailwindcss()],
   },
