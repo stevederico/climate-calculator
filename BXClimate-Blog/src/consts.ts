87f043fefdc533f030b62153a6608a82c821d094
Steve Derico <steve@bixbyapps.com>
2025-04-29 15:20:18 -0700

    removed blog

--- BXClimate-Blog/src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Climate Blog';
-export const SITE_DESCRIPTION = 'A blog about climate change and sustainability';
-export const SITE_COMPANY = 'Company Inc';
